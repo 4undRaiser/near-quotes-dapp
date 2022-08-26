e434ac290dc6c723cdf794f40798a7150746b14b
AshenI99 <asheniranda99@gmail.com>
2022-08-26 11:58:28 +0530

    Fixes + Improvements

--- src/components/marketplace/AddQuote.js
+++ src/components/marketplace/AddQuote.js
@@ -1,76 +1,76 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
-import { Button, Modal, Form, FloatingLabel } from "react-bootstrap";
+import {Button, Modal, Form, FloatingLabel} from "react-bootstrap";
 
-const AddQuote = ({ save }) => {
-  const [description, setDescription] = useState("");
+const AddQuote = ({save}) => {
+    const [description, setDescription] = useState("");
 
-  const isFormFilled = () => description;
+    const isFormFilled = () => description;
 
-  const [show, setShow] = useState(false);
+    const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
-  return (
-    <>
-      <Button
-        onClick={handleShow}
-        variant="dark"
-        className="rounded-pill px-0"
-        style={{ width: "38px" }}
-      >
-        <i class="bi bi-plus"></i>
-      </Button>
-      <Modal show={show} onHide={handleClose} centered>
-        <Modal.Header closeButton>
-          <Modal.Title>New Quote</Modal.Title>
-        </Modal.Header>
-        <Form>
-          <Modal.Body>
-          
-            
-            <FloatingLabel
-              controlId="inputDescription"
-              label="Description"
-              className="mb-3"
+    return (
+        <>
+            <Button
+                onClick={handleShow}
+                variant="dark"
+                className="rounded-pill px-0"
+                style={{width: "38px"}}
             >
-              <Form.Control
-                as="textarea"
-                placeholder="description"
-                style={{ height: "80px" }}
-                onChange={(e) => {
-                  setDescription(e.target.value);
-                }}
-              />
-            </FloatingLabel>
-            
-          </Modal.Body>
-        </Form>
-        <Modal.Footer>
-          <Button variant="outline-secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button
-            variant="dark"
-            disabled={!isFormFilled()}
-            onClick={() => {
-              save({
-                description,
-              });
-              handleClose();
-            }}
-          >
-            Save product
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
+                <i class="bi bi-plus"></i>
+            </Button>
+            <Modal show={show} onHide={handleClose} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>New Quote</Modal.Title>
+                </Modal.Header>
+                <Form>
+                    <Modal.Body>
+
+
+                        <FloatingLabel
+                            controlId="inputDescription"
+                            label="Description"
+                            className="mb-3"
+                        >
+                            <Form.Control
+                                as="textarea"
+                                placeholder="description"
+                                style={{height: "80px"}}
+                                onChange={(e) => {
+                                    setDescription(e.target.value);
+                                }}
+                            />
+                        </FloatingLabel>
+
+                    </Modal.Body>
+                </Form>
+                <Modal.Footer>
+                    <Button variant="outline-secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button
+                        variant="dark"
+                        disabled={!isFormFilled()}
+                        onClick={() => {
+                            save({
+                                description,
+                            });
+                            handleClose();
+                        }}
+                    >
+                        Save Quote
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
 };
 
 AddQuote.propTypes = {
-  save: PropTypes.func.isRequired,
+    save: PropTypes.func.isRequired,
 };
 
 export default AddQuote;
