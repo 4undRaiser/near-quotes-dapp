360fd553a63eb44587a6fa1ebe81b1b6f9c98ba9
FarzeenKist <farzeenkistnareddy@gmail.com>
2022-08-26 09:32:38 +0400

    fixed bug and improvements

--- smartcontract/assembly/index.ts
+++ smartcontract/assembly/index.ts
@@ -1,62 +1,70 @@
-import { context, u128, ContractPromiseBatch} from "near-sdk-as";
-import { Quote, quoteStorage, Comment, } from './model';
-
-
+import { context, u128, ContractPromiseBatch } from "near-sdk-as";
+import { Quote, quoteStorage, Comment, likedStorage } from "./model";
 
 /**
- * This function will add a new quotes to the mapping
+ * @dev This function will add a new quotes to the mapping
  */
- export function addQuote(quote: Quote): void {
-  let storedQuote = quoteStorage.get(quote.id);
-  if (storedQuote !== null) {
-      throw new Error(`a quote with id=${quote.id} already exists`);
-  }
-  quoteStorage.set(quote.id, Quote.fromPayload(quote));
+export function addQuote(quote: Quote): void {
+	let storedQuote = quoteStorage.get(quote.id);
+	if (storedQuote !== null) {
+		throw new Error(`a quote with id=${quote.id} already exists`);
+	}
+	assert(quote.description.length > 0, "Quote's text can't be empty");
+	quoteStorage.set(quote.id, Quote.fromPayload(quote));
 }
 
 /**
- * this function will return an a quote @index id
+ * @dev this function will return an a quote @index id
  */
- 
- export function getQuote(id: string): Quote | null {
-  return quoteStorage.get(id);
+
+export function getQuote(id: string): Quote | null {
+	return quoteStorage.get(id);
 }
 
 /**
-* 
-* Returns all quotes in the mapping
-*/
+ *
+ * @dev Returns all quotes in the mapping
+ */
 export function getQuotes(): Array<Quote> {
-  return quoteStorage.values();
+	return quoteStorage.values();
 }
 
-
-// faclitates users adding a comment to quote
-
+/**
+ *  @dev faclitates users adding a comment to quote
+ */
 export function addComment(id: string, comment: Comment): void {
-  let storedQuote = quoteStorage.get(id);
-  if (storedQuote == null) {
-      throw new Error(`a quote with id=${id} does not exists`);
-  }
-  if (storedQuote.owner == context.sender ){
-    throw new Error(" You cannot comment on your own quote")
-  }
-  storedQuote.comments.push(comment);
+	let storedQuote = quoteStorage.get(id);
+	if (storedQuote == null) {
+		throw new Error(`a quote with id=${id} does not exists`);
+	}
+  assert(storedQuote.owner != context.sender, "You cannot comment on your own quote");
+  assert(comment.commentDescription.length > 0, "Comment can't be empty");
+	storedQuote.addComment(comment);
 }
-  
 
-// facilitates user liking a quote and supporting the author with any ammount
+/**
+ * @dev facilitates user liking a quote and supporting the author with any amount
+ * @notice users can like a quote only once
+ */
 export function likeQuote(id: string): void {
-  const quote = getQuote(id);
-  if (quote == null) {
-      throw new Error("quote not found");
-  }
-
-  if (quote.owner == context.sender) {
-    throw new Error("can't like your own quote");
-}
-
-  ContractPromiseBatch.create(quote.owner).transfer(context.attachedDeposit);
-  quote.incrementLikes();
-  quoteStorage.set(id, quote);
+	const quote = getQuote(id);
+	const liked = likedStorage.get(context.sender);
+	if (quote == null) {
+		throw new Error("");
+	}
+	assert(quote.owner != context.sender, "can't like your own quote");
+	if (liked == null) {
+		likedStorage.set(context.sender, [id]);
+	} else {
+		assert(liked.indexOf(id) == -1, "Already liked");
+		liked.push(id);
+		likedStorage.set(context.sender, liked);
+	}
+	if (context.attachedDeposit > u128.Min) {
+		ContractPromiseBatch.create(quote.owner).transfer(
+			context.attachedDeposit
+		);
+	}
+	quote.incrementLikes(context.attachedDeposit);
+	quoteStorage.set(id, quote);
 }
--- smartcontract/assembly/model.ts
+++ smartcontract/assembly/model.ts
@@ -1,49 +1,51 @@
-import { PersistentUnorderedMap, context, PersistentMap, u128 } from "near-sdk-as";
-
+import { PersistentUnorderedMap, context, u128 } from "near-sdk-as";
 
 @nearBindgen
 export class Quote {
-    id: string;
-    description: string;
-    owner: string;
-    likes: u32;
-    comments: Array<Comment>;
+	id: string;
+	description: string;
+	owner: string;
+	likes: u32;
+	comments: Array<Comment>;
+    donations: u128;
+	public static fromPayload(payload: Quote): Quote {
+		const quote = new Quote();
+		quote.id = payload.id;
+		quote.description = payload.description;
+		quote.owner = context.sender;
+        quote.likes = 0;
+		quote.comments = [];
+        quote.donations = u128.Zero;
+		return quote;
+	}
 
-    public static fromPayload(payload: Quote): Quote {
-        const quote = new Quote();
-        quote.id = payload.id;
-        quote.description = payload.description;
-        quote.owner = context.sender;
-        quote.comments = [];
-        return quote;
-    }
+	public incrementLikes(donation: u128): void {
+		this.likes = this.likes + 1;
+        this.donations = u128.add(this.donations, donation);
+	}
 
-    public incrementLikes(): void {
-        this.likes = this.likes + 1;
-    }
-
-   
-    
+	public addComment(comment: Comment): void {
+		this.comments.push(comment);
+	}
 }
-
-
 
 @nearBindgen
 export class Comment {
-    commentDescription: string;
-    owner: string;
+	commentDescription: string;
+	owner: string;
 
-    public static fromPayload(payload: Comment): Comment {
-        const comment = new Comment();
-        comment.commentDescription = payload.commentDescription;
-        comment.owner = context.sender;
-        return comment;
-    }
-
-    
+	public static fromPayload(payload: Comment): Comment {
+		const comment = new Comment();
+		comment.commentDescription = payload.commentDescription;
+		comment.owner = context.sender;
+		return comment;
+	}
 }
 
+export const quoteStorage = new PersistentUnorderedMap<string, Quote>(
+	"LISTED_QUOTES"
+);
 
-
-export const quoteStorage = new PersistentUnorderedMap<string, Quote>("LISTED_QUOTES");
-//export const commentStorage = new PersistentUnorderedMap<string, Array<Comment>>("COMMENTS");
+export const likedStorage = new PersistentUnorderedMap<string, Array<string>>(
+	"USERS_LIKED"
+);
